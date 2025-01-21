9d0fe522f455591f24c4dcfd5a6ecd752ef5e3c0
ste3v0 <steffen.kaiser@stud.htw-dresden.de>
2025-01-21 17:45:55 +0100

    Added bootstrap to shoppingcart

--- WebShop/view/ShoppingCart.js
+++ WebShop/view/ShoppingCart.js
@@ -3,59 +3,49 @@ import { mapMutations, mapGetters, mapState } from 'vuex';
 export default {
   name: 'ShoppingCart',
   template: String.raw`
-    <button class="cart-button" @click="$router.push('/')">
-      <i class="fas fa-arrow-left"></i> Zurück zum Shop
-    </button>
-    <h1>Warenkorb</h1>
-    <table class="table" style="width: 100%; text-align: center;">
-      <thead>
-        <tr>
-          <th style="width: 40%; padding-bottom: 10px;">Artikel</th>
-          <th style="width: 15%; padding-bottom: 10px;">Anzahl</th>
-          <th style="width: 15%; padding-bottom: 10px;">Preis Netto</th>
-          <th style="width: 15%; padding-bottom: 10px;">MwSt</th>
-          <th style="width: 15%; padding-bottom: 10px;">Preis Brutto</th>
-        </tr>
-      </thead>
-      <tr>
-        <td colspan="5" style="padding-bottom: 10px; border-bottom: 1px solid #a3a3a3;"></td>
-      </tr>
-      <tbody>
-        <tr v-for="article in getArticlesInCart" :key="article.prodID">
-          <td style="text-align: center;">{{ article.titel }}</td>
-          <td style="text-align: center;">
-            <button class="btn-increase" @click="removeOneFromCart(article.prodID)">
-              <template v-if="article.amount > 1">-</template>
-              <template v-else><i class="fas fa-trash"></i></template>
-            </button>
-            {{ article.amount }}
-            <button class="btn-decrease" @click="addOneToCart(article.prodID)">+</button>
-          </td>
-          <td style="text-align: center;">{{ (article.amount * article.preis).toFixed(2) }} €</td>
-          <td style="text-align: center;">{{ (article.amount * article.preis * MwStSatz).toFixed(2) }} €</td>
-          <td style="text-align: center;">{{ (article.amount * article.preis * (1 + MwStSatz)).toFixed(2) }} €</td>
-        </tr>
-        <tr></tr>
-        <td colspan="0" style="text-align: center; padding-top: 15px;">
-          <strong>Gesamtbetrag:</strong>
-        </td>
-        <td colspan="1" style="text-align: center; padding-top: 15px;"></td>
-        <td colspan="1" style="text-align: center; padding-top: 15px;">
-          {{ sumNettoOfArticlesInCart.toFixed(2) }} €
-        </td>
-        <td colspan="1" style="text-align: center; padding-top: 15px;">
-          {{ sumMwstOfArticlesInCart.toFixed(2) }} €
-        </td>
-        <td colspan="1" style="text-align: center; padding-top: 15px;">
-          <strong>{{ sumBruttoOfArticlesInCart.toFixed(2)}} €</strong>
-        </td>
-        </tr>
-      </tbody>
-    </table>
-    <div style="text-align: center; margin-top: 30px;">
-      <button class="btn-increase" style="width: auto; padding: 10px 20px;" @click="payment">
-        Bezahlen
+     <div class="container mt-5">
+      <button class="btn btn-secondary mb-3" @click="$router.push('/')">
+        <i class="fas fa-arrow-left"></i> Zurück zum Shop
       </button>
+      <h1>Warenkorb</h1>
+      <table class="table table-bordered text-center">
+        <thead class="thead-light">
+          <tr>
+            <th>Artikel</th>
+            <th>Anzahl</th>
+            <th>Preis Netto</th>
+            <th>MwSt</th>
+            <th>Preis Brutto</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="article in getArticlesInCart" :key="article.prodID">
+            <td>{{ article.titel }}</td>
+            <td>
+              <button class="btn btn-outline-secondary btn-sm" @click="removeOneFromCart(article.prodID)">
+                <template v-if="article.amount > 1">-</template>
+                <template v-else><i class="fas fa-trash"></i></template>
+              </button>
+              {{ article.amount }}
+              <button class="btn btn-outline-secondary btn-sm" @click="addOneToCart(article.prodID)">+</button>
+            </td>
+            <td>{{ (article.amount * article.preis).toFixed(2) }} €</td>
+            <td>{{ (article.amount * article.preis * MwStSatz).toFixed(2) }} €</td>
+            <td>{{ (article.amount * article.preis * (1 + MwStSatz)).toFixed(2) }} €</td>
+          </tr>
+          <tr>
+            <td colspan="2"><strong>Gesamtbetrag:</strong></td>
+            <td>{{ sumNettoOfArticlesInCart.toFixed(2) }} €</td>
+            <td>{{ sumMwstOfArticlesInCart.toFixed(2) }} €</td>
+            <td><strong>{{ sumBruttoOfArticlesInCart.toFixed(2) }} €</strong></td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="text-center mt-4">
+        <button class="btn btn-primary" @click="payment">
+          Bezahlen
+        </button>
+      </div>
     </div>
     `,
   
